f9b923594c468c70cf975854666782dcdfdc4ec8
sbengezal <sbengezal@beeline.ru>
2023-07-16 18:34:55 +0300

    Reformat code

--- hw9/src/main/java/ru/otus/homeworks/hw9/service/BookService.java
+++ hw9/src/main/java/ru/otus/homeworks/hw9/service/BookService.java
@@ -1,8 +1,8 @@
 package ru.otus.homeworks.hw9.service;
 
 import ru.otus.homeworks.hw9.dto.BookDtoResponse;
-import ru.otus.homeworks.hw9.dto.UpdateBookDtoRequest;
 import ru.otus.homeworks.hw9.dto.NewBookDtoRequest;
+import ru.otus.homeworks.hw9.dto.UpdateBookDtoRequest;
 import ru.otus.homeworks.hw9.exceptions.EntityNotFoundException;
 
 import java.util.List;
